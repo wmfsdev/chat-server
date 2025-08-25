11c6bf568dfa229eea056b445d86b34e8ad6e37b
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2025-08-25 23:48:38 +0100

    switch to cjs syntax

--- socket-instance.js
+++ socket-instance.js
@@ -1,18 +1,18 @@
-import { Server } from 'socket.io';
-import { createServer } from 'http';
-import express from 'express';
-import cors from 'cors'
+
+const express = require('express')
+const { createServer } = require("http")
+const { Server } = require('socket.io');
+const cors = require("cors")
 
 const app = express();
 const server = createServer(app);
 
-app.use(cors())
+app.use(cors());
 
 const io = new Server(server, {
-    cors: {
-        origin: 'http://localhost:5173'
-    }
+  cors: {
+    origin: "http://localhost:5173",
+  },
 });
 
-
-export { app, io, server };
+module.exports = { app, io, server };
