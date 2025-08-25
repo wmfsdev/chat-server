8325ad63246e5868cc06800c5ca22391de3990c2
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2025-08-25 23:47:02 +0100

    switch to cjs syntax

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
-import { app, io, server } from "./socket-instance.js";
-import "./connection.js";
-import loginRouter from "./routes/login.js";
+const { app, io, server } = require('./socket-instance');
+const express = require('express')
+
+require('./connection')
+
+const accountRouter = require('./routes/login');
+
+require('dotenv').config();
+
+app.use(express.json());
 
 app.use((req, res, next) => {
   req.io = io;
   return next();
 });
 
-app.use("/", loginRouter);
+app.use("/", accountRouter);
+
+app.use((err, req, res, next) => {
+  console.log("express err handler")
+  console.error(err);
+  res.status(500).send(err);
+});
 
 server.listen(3001, () => {
   console.log("server running");
