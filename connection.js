3a5c7bfba988e02cdc482cb89e5646b5b0da6f3c
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2025-09-12 20:54:04 +0100

    create new socket event listeners for disconnects, user connections and private messages

--- connection.js
+++ connection.js
@@ -7,42 +7,55 @@ const jwt = require('jsonwebtoken')
 const connect = io.of("/profile");
 
 connect.use((socket, next) => {
+  // token auth
   const token = socket.handshake.auth.token
-  const decoded = jwt.verify(token, process.env.SECRET)
-  socket.data.username = decoded.username
-  next()
+
+  jwt.verify(token, process.env.SECRET, function(err, decoded) {
+    if (err) {
+      console.log("error: ", err)
+      next(err)
+    } else {
+      console.log("decoded: ", decoded)
+      socket.data.username = decoded.username
+      socket.data.id = decoded.id
+      next()
+    }
+  })
 })
 
 
 connect.on("connection", async (socket) => {
-  console.log("connection", socket.id)
+  console.log("connected", socket.id)
 
   const users = []
   const sockets = await connect.fetchSockets()
   
   for ( const socket of sockets) {
     users.push({
-      userID: socket.id,
+      socketID: socket.id,
+      userID: socket.data.id,
       username: socket.data.username
     })
   }
-
+  console.log("USERS: ", users)
   socket.emit("users", users)
-
-  socket.on("users", (socket) => {
-  })
+  
+  socket.broadcast.emit("user_connected", {
+      socketID: socket.id,
+      userID: socket.data.id,
+      username: socket.data.username
+  });
 
   // DISCONNECT
-  socket.on("disconnect", (socket) => {
-    console.log("disconnected")
+  socket.on("disconnecting", async () => {
+    console.log(`user disconnecting...`)
+    console.log(socket.data)
+    console.log(socket.rooms)
+    const username = await socket.data.username
+    socket.broadcast.emit("user_disconnected", {
+      username: username
+    })
   })
-
-  // SET USER - redundant?
-  // socket.on("set_user", (username) => {
-  //   // pass in client supplied username from Profile
-  //   console.log("set_user: ", username)
-  //   socket.data.username = username
-  // })
 
   // JOIN ROOM
   socket.on("join_room", (id, data) => {
@@ -52,10 +65,12 @@ connect.on("connection", async (socket) => {
     socket.to(id).emit("user_joined", data.username)
   })
 
-  // SEND MESSAGE
-  socket.on("send_message", async (data) => {
+  // SEND PRIVATE MESSAGE
+  socket.on("send_priv_message", (data) => {
     console.log(data)
-
-    socket.to(data.room).emit("receive_message", { content: data, from: socket.id })
+    socket.to(data.to).emit("receive_priv_message", { id: data.id, from: data.from, message: data.message })
   })
 });
+
+
+
