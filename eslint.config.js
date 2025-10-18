d06c8adf7a049a907649f798b9ff0bbb81a8c065
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2025-10-18 23:40:37 +0100

    **

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,11 @@ import { defineConfig } from "eslint/config";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
-  eslintConfigPrettier
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.node },
+  },
+  eslintConfigPrettier,
 ]);
