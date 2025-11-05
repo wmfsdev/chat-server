d9d4273fcac581fcf6362f7558d58b359c102aab
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2025-11-05 00:17:28 +0000

    update env variable name

--- prisma/client.js
+++ prisma/client.js
@@ -2,7 +2,7 @@ const { PrismaClient } = require('../generated/prisma')
 
 const databaseUrl = process.env.NODE_ENV === 'dev'
   ? process.env.TEST_DB_URL
-  : process.env.DB_URL;
+  : process.env.DATABASE_URL;
 
 const prisma = new PrismaClient({
   datasources: {
