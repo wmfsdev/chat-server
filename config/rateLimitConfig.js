7834e28a6a2627ec31f1ce4ad180188966bef04f
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2025-11-05 21:57:54 +0000

    create dedicated configs for rate limiters

---
+++ config/rateLimitConfig.js
@@ -0,0 +1,19 @@
+const rateLimit = require('express-rate-limit')
+
+const loginLimiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 15 minutes
+  limit: 5, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
+})
+
+const signupLimiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 15 minutes
+  limit: 1, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
+})
+
+module.exports = { loginLimiter, signupLimiter };
